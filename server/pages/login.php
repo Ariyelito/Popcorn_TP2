f315aead6182f84e4157c980e61269dd928ebc68
harfy345 <56700113+harfy345@users.noreply.github.com>
2021-10-09 19:41:24 -0400

    Update login.php

--- server/pages/login.php
+++ server/pages/login.php
@@ -10,43 +10,68 @@ include '../../includes/header-m.php';
     $emailEntree = $_POST['email'];
     $passwordEntree = $_POST['mdp'];
 
-    $connectionFile = fopen("../data/connection.txt", "r") or exit("Unable to open file!");
+   // $connectionFile = fopen("../data/connection.txt", "r") or exit("Unable to open file!");
 
     $found = false;
     
     $isMembre;
-    while (!feof($connectionFile) && !$found) {
-        $ligne = fgets($connectionFile) . "<br />";
-      //  $tabLigne = explode(";", "$ligne;");
-      list($mailDB, $hash,$isActive,$isMembre) = explode(";", "$ligne");
 
-        if ($emailEntree == $mailDB) {
-            if (password_verify($passwordEntree, $hash)) {
+    require_once '../../db/connexion.inc.php';
 
-                $found = true;
-            }
-        }
-    }
-
-
-    if (!$found) {
-        echo "Verifier le mot de passe ou l'email";
-        echo "<br /><a href=\"../../index.php\">Accueil</a>";
-
-    } else {        
+    $result = $crudMembre->getMembreByEmail($emailEntree);
+    if(!$result)
+    echo "Not found";
+    else {
+        if($passwordEntree == $result['password']){
         echo "<h3>Vous êtes connecté ($emailEntree)</h3>";
       
-         if($isMembre =='M')
+         if($result['role']=='M')
          {
          header('Location: ../../membrePage.php');
          }
-        else if($isMembre =='A')
+        else if($result['role'] =='A')
         {
             header('Location: ../../admin.php');
         }
     
-    } 
-    fclose($connectionFile);
+        }else{
+            echo "password ne pas bon";
+        }
+    }
+     // $r = $result->fetch(PDO::FETCH_ASSOC);
+    // echo $r['prenom'];
+    // while (!feof($connectionFile) && !$found) {
+    //     $ligne = fgets($connectionFile) . "<br />";
+    //   //  $tabLigne = explode(";", "$ligne;");
+    //   list($mailDB, $hash,$isActive,$isMembre) = explode(";", "$ligne");
+
+    //     if ($emailEntree == $mailDB) {
+    //         if (password_verify($passwordEntree, $hash)) {
+
+    //             $found = true;
+    //         }
+    //     }
+    // }
+
+
+    // if (!$found) {
+    //     echo "Verifier le mot de passe ou l'email";
+    //     echo "<br /><a href=\"../../index.php\">Accueil</a>";
+
+    // } else {        
+    //     echo "<h3>Vous êtes connecté ($emailEntree)</h3>";
+      
+    //      if($isMembre =='M')
+    //      {
+    //      header('Location: ../../membrePage.php');
+    //      }
+    //     else if($isMembre =='A')
+    //     {
+    //         header('Location: ../../admin.php');
+    //     }
+    
+    // } 
+    // fclose($connectionFile);
     ?>
 </div>
 <?php include '../../includes/footer.php' ?>
