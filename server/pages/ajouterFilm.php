5e9eaba56fa2b2f15068a8f65be4a9eccfb0cd75
harfy345 <56700113+harfy345@users.noreply.github.com>
2021-10-10 18:16:45 -0400

    Update ajouterFilm.php
    j'ai ajouté le code pour ajouterFilm.php

---
+++ server/pages/ajouterFilm.php
@@ -0,0 +1,34 @@
+
+
+<?php
+$title = "admin";
+// include '../../includes/header-a.php';
+require_once '../../db/connexion.inc.php';
+?>
+<div class="container mt-3">
+<?php
+
+$titre = $_POST['inputTitre'];
+$date = $_POST['inputDate'];
+$langue = $_POST['langue'];
+$montant = $_POST['inputCout'];
+$duree = $_POST['inputDure'];
+$photo = "";
+$idCategories = array(1,2);
+$idRealisateurs = array(1,2);
+
+
+$film = new Film(0,$titre,$duree,$langue,$date,$montant,$photo,$idCategories,$idRealisateurs);
+
+$crud->addFilm($film);
+$film->idFilm = $pdo->lastInsertId();
+
+$crud->addCategoriesPourFilm($film->idFilm,$idCategories);
+$crud->addRealisateursPourFilm($film->idFilm,$idRealisateurs);
+
+echo ("Le film ".$film->idFilm." a été bien ajouté à la base de données.");
+?>
+</div>
+<?php  include '../../includes/footer-membre.php'?>
+
+
