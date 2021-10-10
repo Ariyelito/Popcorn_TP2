0d583bcef3dc4be30627a133a5a08412d5489017
harfy345 <56700113+harfy345@users.noreply.github.com>
2021-10-10 18:15:50 -0400

    Update crud.php
    ajouter les méthodes pour ajouter un film

--- db/crud.php
+++ db/crud.php
@@ -21,8 +21,74 @@ class crud
             return false;
         }
     }
-    public function addMovie(){
+    public function addFilm(Film $film ){
+        try {
+            // define sql statement to be executed
+            $sql = "INSERT INTO films (titre,duree,langue,date,montant,photo)
+                    VALUES(:titre,:duree,:langue,:date,:montant,:photo)";
+            // prepare the sql statement for execution
+            $stmt = $this->db->prepare($sql);
+            // bind all placeholders to the actual values
+            $stmt->bindparam(':titre', $film->titre);
+            $stmt->bindparam(':duree', $film->duree);
+            $stmt->bindparam(':langue', $film->langue);
+            $stmt->bindparam(':date', $film->date);
+            $stmt->bindparam(':montant', $film->montant);
+            $stmt->bindparam(':photo', $film->photo);
 
+            // execute statement
+            $stmt->execute();
+          
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+    public function addRealisateursPourFilm($idFilm, $realisateurs){
+      
+
+
+        try {
+            
+            $sql = "INSERT INTO `filmsrealisateurs` (`idFilm`, `idRealisateur`) VALUES ";
+           
+           foreach($realisateurs as $id){
+            $sql.="($idFilm,$id),";
+           }
+           $sql = substr($sql, 0, -1);
+
+            $stmt = $this->db->prepare($sql);
+            // execute statement
+            $stmt->execute();
+          
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+    public function addCategoriesPourFilm($idFilm, $categories){
+      
+
+
+        try {
+            
+            $sql = "INSERT INTO `filmcategorie` (`idFilm`, `idCategorie`) VALUES ";
+           
+           foreach($categories as $id){
+            $sql.="($idFilm,$id),";
+           }
+           $sql = substr($sql, 0, -1);
+            $stmt = $this->db->prepare($sql);
+            // execute statement
+            $stmt->execute();
+          
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
     }
 
 }
