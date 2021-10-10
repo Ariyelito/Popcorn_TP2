e27f529308761f07451e987e7c3454f87b10a72f
harfy345 <56700113+harfy345@users.noreply.github.com>
2021-10-10 18:51:59 -0400

    Update lister.php
    adapter le fichier lister.php aux changements

--- server/pages/lister.php
+++ server/pages/lister.php
@@ -37,104 +37,148 @@
 
 
 <?php
+
+
+require_once '../../db/connexion.inc.php';
+
+$result = $crudMembre->getAllMembres();
+$result = $result->fetchAll();
+
 echo '<h2 style="text-align:center;color:blue;">LISTE DES MEMBRES</h2>';
+$rep = '<table class="table table-striped">';
+$rep .= '<thead><tr><th scope="col">Prénom</th> <th scope="col">Nom</th>';
+$rep .= '<th scope="col">Courriel</th> <th scope="col">Statut</th>';
+$rep .= '<th scope="col">Rôle</th> </tr></thead>';
 
-define("FICHIER_MEMBRES", "../data/membres.txt");
-define("FICHIER_CONNEXION", "../data/connection.txt");
 
-$tabInfos = array();
+$rep .= "<tbody>";
 
-if (($ficMembres = fopen(FICHIER_MEMBRES, "r")) === null || ($ficConnexion = fopen(FICHIER_CONNEXION, "r")) === null) {
-    echo "<p>Nous avons un problème pour créer/ouvrir les fichiers</p>";
-    exit;
+foreach($result as $membre){
+    $connexion = $crudMembre->getMembreConnexion($membre['email']);
+    $rep .= "<tr>";
+    $rep .= "<td>".$membre['prenom']."</td>";
+    $rep .= "<td>".$membre['nom']."</td>";
+    $rep .= "<td>".$membre['email']."</td>";
+    $rep .= "<td>".getStatut($connexion['statue'])."</td>";
+    $rep .= "<td>".getRole($connexion['role'])."</td>";
+  
+    $rep .="</tr>";
 }
 
-function infosConnexion($courriel)
-{
-    global $ficConnexion;
-    $ligne = fgets($ficConnexion);
-    while (!feof($ficConnexion)) {
-        $tab = explode(";", $ligne);
-        if ($courriel === $tab[0]) {
-            $infos = $tab[2] . ";" . $tab[3];
-            return $infos;
-        } else {
-            $ligne = fgets($ficConnexion);
-        }
-    }
 
-    return "";
+
+$rep .= "</tbody>";
+
+echo $rep;
+
+function getStatut($statut){
+    if($statut ==0){
+        return "inactif";
+    }else return "actif";
 }
+function getRole($role){
 
-function detailInfos($tab2)
-{
+    if($role == 'A')
+        return "Admin";
+    else if($role == 'M')  return "Membre";
 
-    $statut = $role = null;
-    switch ((int) $tab2[0]) {
-        case 0:$statut = "Inactif";
-            break;
-        case 1:$statut = "Actif";
-            break;
-    }
-    $tab2[0] = $statut;
-
-    switch (trim($tab2[1])) {
-        case "M":$role = "Membre";
-            break;
-        case "A":$role = "Administrateur";
-            break;
-        case "E":$role = "Employé";
-            break;
-    }
-    $tab2[1] = $role;
-    return $tab2;
 }
+// define("FICHIER_MEMBRES", "../data/membres.txt");
+// define("FICHIER_CONNEXION", "../data/connection.txt");
 
-function obtenirTableauInfosMembres()
-{
-    global $tabInfos, $ficMembres, $ficConnexion;
-    $ligne = fgets($ficMembres);
-    while (!feof($ficMembres)) {
-        $tab = explode(";", $ligne);
-        $infos = infosConnexion(trim($tab[2]));
-        $tab = [$tab[0], $tab[1], $tab[2]];
-        $tab2 = explode(";", $infos);
-        $tab2 = detailInfos($tab2);
+// $tabInfos = array();
 
-        $tab = array_merge($tab, $tab2);
+// if (($ficMembres = fopen(FICHIER_MEMBRES, "r")) === null || ($ficConnexion = fopen(FICHIER_CONNEXION, "r")) === null) {
+//     echo "<p>Nous avons un problème pour créer/ouvrir les fichiers</p>";
+//     exit;
+// }
 
-        $tabInfos[] = $tab;
-        $ligne = fgets($ficMembres);
+// function infosConnexion($courriel)
+// {
+//     global $ficConnexion;
+//     $ligne = fgets($ficConnexion);
+//     while (!feof($ficConnexion)) {
+//         $tab = explode(";", $ligne);
+//         if ($courriel === $tab[0]) {
+//             $infos = $tab[2] . ";" . $tab[3];
+//             return $infos;
+//         } else {
+//             $ligne = fgets($ficConnexion);
+//         }
+//     }
 
-    }
+//     return "";
+// }
 
-    fclose($ficMembres);
-    fclose($ficConnexion);
-}
+// function detailInfos($tab2)
+// {
 
-function retournerTableauHTMLMembres()
-{
-    global $tabInfos;
-    obtenirTableauInfosMembres();
+//     $statut = $role = null;
+//     switch ((int) $tab2[0]) {
+//         case 0:$statut = "Inactif";
+//             break;
+//         case 1:$statut = "Actif";
+//             break;
+//     }
+//     $tab2[0] = $statut;
 
-    $rep = '<table class="table table-striped">';
-    $rep .= '<thead><tr><th scope="col">Prénom</th> <th scope="col">Nom</th>';
-    $rep .= '<th scope="col">Courriel</th> <th scope="col">Statut</th>';
-    $rep .= '<th scope="col">Rôle</th> </tr></thead>';
+//     switch (trim($tab2[1])) {
+//         case "M":$role = "Membre";
+//             break;
+//         case "A":$role = "Administrateur";
+//             break;
+//         case "E":$role = "Employé";
+//             break;
+//     }
+//     $tab2[1] = $role;
+//     return $tab2;
+// }
 
-    $rep .= "<tbody>";
-    foreach ($tabInfos as $unMembre) {
-        $rep .= "<tr>";
-        foreach ($unMembre as $valeur) {
-            $rep .= "<td >" . $valeur . "</td>";
-        }
-        $rep .= "</tr>";
-    }
-    unset($tabInfos);
-    $rep .= "</tbody></table>";
-    echo $rep;
-}
-retournerTableauHTMLMembres();
+// function obtenirTableauInfosMembres()
+// {
+//     global $tabInfos, $ficMembres, $ficConnexion;
+//     $ligne = fgets($ficMembres);
+//     while (!feof($ficMembres)) {
+//         $tab = explode(";", $ligne);
+//         $infos = infosConnexion(trim($tab[2]));
+//         $tab = [$tab[0], $tab[1], $tab[2]];
+//         $tab2 = explode(";", $infos);
+//         $tab2 = detailInfos($tab2);
+
+//         $tab = array_merge($tab, $tab2);
+
+//         $tabInfos[] = $tab;
+//         $ligne = fgets($ficMembres);
+
+//     }
+
+//     fclose($ficMembres);
+//     fclose($ficConnexion);
+// }
+
+// function retournerTableauHTMLMembres()
+// {
+//     global $tabInfos;
+//     obtenirTableauInfosMembres();
+
+//     $rep = '<table class="table table-striped">';
+//     $rep .= '<thead><tr><th scope="col">Prénom</th> <th scope="col">Nom</th>';
+//     $rep .= '<th scope="col">Courriel</th> <th scope="col">Statut</th>';
+//     $rep .= '<th scope="col">Rôle</th> </tr></thead>';
+
+//     $rep .= "<tbody>";
+//     foreach ($tabInfos as $unMembre) {
+//         $rep .= "<tr>";
+//         foreach ($unMembre as $valeur) {
+//             $rep .= "<td >" . $valeur . "</td>";
+//         }
+//         $rep .= "</tr>";
+//     }
+//     unset($tabInfos);
+//     $rep .= "</tbody></table>";
+//     echo $rep;
+// }
+// retournerTableauHTMLMembres();
 ?>
     <div class="container mt-5">
         <p>
