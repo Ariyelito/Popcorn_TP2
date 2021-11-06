6e9ae4a1b4be3c7b78e5ee09a798ed1add65342a
RAnas20111 <91265890+RAnas20111@users.noreply.github.com>
2021-11-06 19:31:22 -0400

    fix bug panier

--- server/pages/listerPanier.php
+++ server/pages/listerPanier.php
@@ -91,7 +91,7 @@ require_once '../../db/connexion.inc.php';
 
 <!-- option de jour-->
 <?php 
-function optionDejour($idFilmP,$prix){  
+function optionDejour($idFilmP,$prixFilm){  
     global $crud;
  
     
@@ -129,13 +129,12 @@ function optionDejour($idFilmP,$prix){
         $rep .= "<td>";
         
         
-        // $result = $crud->getPrixPourUnFilm($journee);
-        // $r = $result->fetch(PDO::FETCH_ASSOC);
-        // $prix=  $r["montant"] * $prix;
-        // $rep .= $prix;
-        // $_SESSION["total"] += $prix;
+        $prixJour = $crud->getPrixPourUnFilm($journee);
+      
+        $prix=  $prixJour*$prixFilm;
+        $rep .= $prix;
+        $_SESSION["total"] += $prix;
         $rep .= "</td>"; 
-
     
 return $rep;
 }
