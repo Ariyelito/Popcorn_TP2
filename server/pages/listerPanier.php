6046aea0b0ecf8c5cc9dd1385561b2f968d96f67
Ariyelito <orlandovillanueva11@gmail.com>
2021-11-06 21:23:57 -0400

    added retour btn to listPanier
    improved code visibility

--- server/pages/listerPanier.php
+++ server/pages/listerPanier.php
@@ -20,77 +20,72 @@ require_once '../../db/connexion.inc.php';
 
  $_SESSION["total"]=0;
 ?>
-   
+
 <h1 class="h1 text-center mt-5">Votre panier</h2>
-<div id="contListMembre" class="container mt-3">
+<div id="contListPanier" class="container mt-3">
     <table class="table table-striped table-hover table-borderless">
         <thead class="table-danger">
-            <th scope="col">img</th> 
-            <th scope="col">Titre</th>  
+            <th scope="col">img</th>
+            <th scope="col">Titre</th>
             <th scope="col">langue</th>
             <th scope="col">Nombre de jour pour la location</th>
             <th scope="col">Montant</th>
             <th scope="col">Retirer</th>
-           
         </thead>
         <tbody>
-<?php
+            <?php
+            $idMembre = $_SESSION['idMembre'];
+            $panier  = $crud->getPanierParIdMembre($idMembre)->fetch();
+            $idPanier= $panier['idPanier'];
+
+            $result = $crud->getPanier($idPanier);
+
+        
+            while($r = $result->fetch(PDO::FETCH_ASSOC)) { 
+                
+                $unFilm= $crud->getFilmById($r['idFilm'])->fetch();
+                ?>
+                        <!-- interface du panier -->
+                <tr>
+                    <td><?php echo $r['idFilm']?></td>
+                    <td><?php echo $unFilm['titre']?></td>
+                    <td><?php echo $unFilm['langue']?></td>
+                    <td>
+                        <!-- listerPanier.php?journee=7 -->
+                        <form action='' id='formPanier<?php echo $r['idFilm']?>' method="POST">
+                            <?php echo optionDejour($r['idFilm'],$unFilm['montant'])?>
+
+                        </form>
+                    </td>
+                    <td> 
+                        <a onclick="return confirm('Are you sure want to delete this record?')"
+                            href="retirerUnfilmDuPanier.php?idFilm=<?php echo $r['idFilm']?>"
+                            class="btn bg-gradient btn-outline-danger">Delete
+                        </a>
+                    </td>
+
+
+                </tr>
+            <?php } ?>
+                <tr>
+                    <td>Total:</td>
+                    <td>Le total Est <?php echo $_SESSION["total"] ?></td>
+                </tr>
+        </tbody>
+    </table>
+    <a id="btnRetourPanier" class="btn btn-danger bg-gradient" href="../../membrePage.php">Retour</a>
+     <a id="btnPayPanier" class="btn btn-success bg-gradient" href='payer.php'>Payer</a>
+            
     
 
 
-    $idMembre = $_SESSION['idMembre'];
-    $panier  = $crud->getPanierParIdMembre($idMembre)->fetch();
-    $idPanier= $panier['idPanier'];
-
-    $result = $crud->getPanier($idPanier);
-
-   
-    while($r = $result->fetch(PDO::FETCH_ASSOC)) { 
-        
-        $unFilm= $crud->getFilmById($r['idFilm'])->fetch();
-        
-        ?>
-    <!-- interface du panier -->
-
-
-            <tr>                       
-                <td><?php echo $r['idFilm']?></td>              
-                <td><?php echo $unFilm['titre']?></td> 
-                <td><?php echo $unFilm['langue']?></td>    
-                <td> 
-<!-- listerPanier.php?journee=7 -->
-                <form  action='' id='formPanier<?php echo $r['idFilm']?>' method="POST">
-                 <?php echo optionDejour($r['idFilm'],$unFilm['montant'])?>             
-               
-                </form>
-    </td>   
-               
-                 <td> <a onclick="return confirm('Are you sure want to delete this record?')" 
-                            href="retirerUnfilmDuPanier.php?idFilm=<?php echo $r['idFilm']?>" class="btn bg-gradient btn-outline-danger">Delete
-                        </a></td>
-               
-               
-            </tr>
-
-            <?php } 
-          
-            
-            ?>
-
-            <tr><td>Total:</td>
-            <td>Le total Est <?php echo $_SESSION["total"] ?></td>
-        </tr> 
-        </tbody>
-    </table>
-    <a id="btnUpdate" class="btn btn-outline-success bg-gradient" href='payer.php'> >Payer</a>
-   
 </div>
 
 
 
 
-<!-- option de jour-->
-<?php 
+    <!-- option de jour-->
+    <?php 
 function optionDejour($idFilmP,$prixFilm){  
     global $crud;
  
@@ -139,11 +134,6 @@ function optionDejour($idFilmP,$prixFilm){
 return $rep;
 }
 ?>
-
-
-
-
-
 <?php
 include '../../includes/footer.php';
 ?>
