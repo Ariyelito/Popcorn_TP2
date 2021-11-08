d259849824254901766f6448260965c4b1e54da9
Ariyelito <orlandovillanueva11@gmail.com>
2021-11-07 21:40:25 -0500

    add fixed height for cards

--- includes/cards.php
+++ includes/cards.php
@@ -14,7 +14,7 @@
         while($r = $result->fetch(PDO::FETCH_ASSOC)) { ?>
 
         <div class="col mt-3">
-            <div id="cardFilm" class="card"  >
+            <div id="cardFilm" class="card">
                 <img src="<?php echo $r['photo'] ?>" class="card-img-top" alt="poster officiel du film">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $r['titre'] ?></h5>
